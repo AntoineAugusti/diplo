3a731ec218035fad505883eb3f4f300f29161100
etiennebatise <etienne.batise@gmail.com>
2015-04-04 22:39:11 +0200

    Ajout de la classe CaseTerrestre.php

---
+++ application/serveur/app/Cartes/CaseTerrestre.php
@@ -0,0 +1,19 @@
+<?php namespace diplo\Cartes;
+
+/**
+ * Class CaseTerrestre
+ */
+class CaseTerrestre
+{
+
+    public function estMaritime()
+    {
+        return false;
+    }
+
+    public function estTerrestre()
+    {
+        return true;
+    }
+}
+
