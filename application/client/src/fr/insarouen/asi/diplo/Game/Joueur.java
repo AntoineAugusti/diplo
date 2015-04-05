e4d0771c5604154aec291c6b761854167a3bc3dd
Nathan Malo <nathan.malo@gmail.com>
2015-04-05 17:02:26 +0200

    Élaboration de la classe permettant de décrire localement un joueur

---
+++ application/client/src/fr/insarouen/asi/diplo/Game/Joueur.java
@@ -0,0 +1,18 @@
+package fr.insarouen.asi.diplo.Game;
+
+
+public class Joueur{
+	public int id;
+	public String pseudo;
+	public String pays;
+	public int armees_restantes;
+	public int cases_controlees;
+
+	public Joueur(int id, String pseudo, String pays, int armees, int cases){
+		this.id=id;
+		this.pseudo=pseudo;
+		this.pays=pays;
+		this.armees_restantes=armees;
+		this.cases_controlees=cases;
+	}
+}
