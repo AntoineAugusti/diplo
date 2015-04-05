4dd215e27f20a0c66c65767c0d1fb15aea9f4b3e
Nathan Malo <nathan.malo@gmail.com>
2015-04-05 17:00:16 +0200

    Ajout des méthodes nécessaire au référencement des joueurs en local

--- application/client/src/fr/insarouen/asi/diplo/Game/Jeu.java
+++ application/client/src/fr/insarouen/asi/diplo/Game/Jeu.java
@@ -1,5 +1,6 @@
 package fr.insarouen.asi.diplo.Game;
 
+import java.util.*;
 
 
 // Il est utile de structurer les informations que l'on reçoit, typiquement une partie.
@@ -8,13 +9,13 @@ public class Jeu{
 	private int id;
 	private int nb_joueurs_requis;
 	private int nb_joueurs_inscrits;
-	// private ArrayList<Joueur> listeDesJoueurs;
+	private HashMap<String,Joueur> listeDesJoueurs;
 
 	public Jeu(int id, int requis, int inscrits){
 		this.id=id;
 		this.nb_joueurs_requis=requis;
 		this.nb_joueurs_inscrits=inscrits;
-		// this.listeDesJoueurs = new ArrayList<Joueur>();
+		this.listeDesJoueurs = new HashMap<String,Joueur>();
 	}
 
 	public int getID(){
@@ -26,8 +27,12 @@ public class Jeu{
 	public int getInscrits(){
 		return nb_joueurs_inscrits;
 	}	
-	// public int getJoueurs(){
-	// 	return listeDesJoueurs;
-	// }
+	public HashMap<String,Joueur> getJoueurs(){
+	 	return listeDesJoueurs;
+	}
+	public boolean miseAJourJoueur(Joueur nouveau){
+		listeDesJoueurs.put(nouveau.pseudo,nouveau);
+		return true;
+	}
 
 }
