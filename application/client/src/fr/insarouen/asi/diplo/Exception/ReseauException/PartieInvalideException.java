c1aaaf4213e4e264abe88721263c5084ecf3e9c8
Nathan Malo <nathan.malo@gmail.com>
2015-04-11 17:10:04 +0200

    Ajout de l'exception PartieInvalideException nécessaire en cas du code HTTP 405 pour récupérer les infos d'une phase

---
+++ application/client/src/fr/insarouen/asi/diplo/Exception/ReseauException/PartieInvalideException.java
@@ -0,0 +1,8 @@
+package fr.insarouen.asi.diplo.Exception.ReseauException;
+
+public class PartieInvalideException extends Exception {
+
+	public PartieInvalideException(String message){
+		System.out.println(message);
+	}	
+}
