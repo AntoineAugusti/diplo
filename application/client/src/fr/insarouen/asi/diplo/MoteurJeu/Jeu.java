c5953d88c20ecbe10df25aacbe59f7033abcdafd
Nathan Malo <nathan.malo@gmail.com>
2015-04-11 16:46:30 +0200

    Ajout des attributs phase et chrono pour la classe Jeu

--- application/client/src/fr/insarouen/asi/diplo/MoteurJeu/Jeu.java
+++ application/client/src/fr/insarouen/asi/diplo/MoteurJeu/Jeu.java
@@ -10,12 +10,15 @@ public class Jeu{
 	private int nb_joueurs_requis;
 	private int nb_joueurs_inscrits;
 	private HashMap<String,Joueur> listeDesJoueurs;
-
+	private Phase phaseCourante;
+	private int chrono;
 	public Jeu(int id, int requis, int inscrits){
 		this.id=id;
 		this.nb_joueurs_requis=requis;
 		this.nb_joueurs_inscrits=inscrits;
 		this.listeDesJoueurs = new HashMap<String,Joueur>();
+		this.phaseCourante=NULL;
+		this.chrono=0;
 	}
 
 	public int getID(){
@@ -26,7 +29,25 @@ public class Jeu{
 	}	
 	public int getInscrits(){
 		return nb_joueurs_inscrits;
-	}	
+	}
+	public int getChrono(){
+		return chrono;
+	}
+	public boolean setChrono(int nouveauChrono){
+		this.chrono=nouveauChrono;
+		return true;
+	} 
+	public Phase getPhaseCourante(){
+		return phaseCourante;
+	}
+	public boolean setPhaseCourante(Phase nouvellePhase){
+		boolean set=false;
+		if (nouvellePhase!=NULL){
+			this.phaseCourante=nouvellePhase;
+			set=true;
+		}
+		return set;
+	}
 	public HashMap<String,Joueur> getJoueurs(){
 	 	return listeDesJoueurs;
 	}
