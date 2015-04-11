b509dfd463e6da33c929fdcb10e10efbae91babf
Nathan Malo <nathan.malo@gmail.com>
2015-04-11 17:18:57 +0200

    Ajout de la classe Case permettant de représenter une case de la carte

---
+++ application/client/src/fr/insarouen/asi/diplo/MoteurJeu/Case.java
@@ -0,0 +1,17 @@
+package fr.insarouen.asi.diplo.MoteurJeu;
+
+public class Case{
+	public int id;
+	public boolean est_libre;
+	public int id_joueur;
+	public boolean est_occupee;
+	public int id_armee;
+
+	public Case(int id, boolean est_libre, boolean est_occupee, int id_armee){
+		this.id = id;
+		this.est_libre = est_libre;
+		this.id_joueur = id_joueur;
+		this.est_occupee = est_occupee;
+		this.id_armee = id_armee;
+	}
+}
