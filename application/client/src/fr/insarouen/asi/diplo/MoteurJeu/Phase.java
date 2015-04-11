55649c680c0cbf85983a21db2f768f260441053c
Nathan Malo <nathan.malo@gmail.com>
2015-04-11 16:54:57 +0200

    Ajout d'une classe Phase afin de pouvoir gérer facilement les statuts de phase ainsi que les chronomètres

---
+++ application/client/src/fr/insarouen/asi/diplo/MoteurJeu/Phase.java
@@ -0,0 +1,35 @@
+package fr.insarouen.asi.diplo.MoteurJeu;
+
+public class Phase{
+
+	public enum Statut{
+		NEGOCIATION,
+		COMBAT,
+		INACTIF
+	}
+	private Statut statut;
+	private int chrono;
+
+	public Phase(Statut statut, int chrono){
+		this.statut=statut;
+		this.chrono=chrono;
+	}
+	public int getChrono(){
+		return chrono;
+	}
+	public boolean setChrono(int nouveauChrono){
+		this.chrono=nouveauChrono;
+		return true;
+	} 
+	public Statut getStatutPhaseCourante(){
+		return statut;
+	}
+	public boolean setStatutPhaseCourante(Statut nouveauStatut){
+		boolean set=false;
+		if (nouveauStatut!=Phase.Statut.INACTIF){
+			this.statut=nouveauStatut;
+			set=true;
+		}
+		return set;
+	}
+}
