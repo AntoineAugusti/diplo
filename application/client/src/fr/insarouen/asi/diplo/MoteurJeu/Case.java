55e8f0a2826c4770ccf16fa9dce399118626274a
Nathan Malo <nathan.malo@gmail.com>
2015-04-11 18:20:11 +0200

    rectification, j'ai pas percuté que est_libre et est_occupee  faisaient déjà le job. J'ai rectifié.

--- application/client/src/fr/insarouen/asi/diplo/MoteurJeu/Case.java
+++ application/client/src/fr/insarouen/asi/diplo/MoteurJeu/Case.java
@@ -3,19 +3,15 @@ package fr.insarouen.asi.diplo.MoteurJeu;
 public class Case{
 	public int id;
 	public boolean est_libre;
-	public boolean a_joueur;
 	public int id_joueur;
 	public boolean est_occupee;
-	public boolean a_armee;
 	public int id_armee;
 
-	public Case(int id, boolean est_libre, boolean a_joueur, int id_joueur, boolean est_occupee, boolean a_armee, int id_armee){
+	public Case(int id, boolean est_libre, int id_joueur, boolean est_occupee, int id_armee){
 		this.id = id;
 		this.est_libre = est_libre;
-		this.a_joueur = a_joueur;
 		this.id_joueur = id_joueur;
 		this.est_occupee = est_occupee;
-		this.a_armee = a_armee;
 		this.id_armee = id_armee;
 	}
 }
