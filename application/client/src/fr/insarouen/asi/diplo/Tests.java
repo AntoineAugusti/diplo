3079fe1059561bca3407f856062602c6b797a1d7
Nathan Malo <nathan.malo@gmail.com>
2015-04-11 16:09:49 +0200

    Ajout d'une fonction, et passage en assert pour plaire à mon Antoine <3

--- application/client/src/fr/insarouen/asi/diplo/Tests.java
+++ application/client/src/fr/insarouen/asi/diplo/Tests.java
@@ -6,28 +6,37 @@ import java.util.*;
 import org.json.*;
 import fr.insarouen.asi.diplo.MoteurJeu.*;
 import java.util.*;
-
+import java.lang.ClassLoader;
 
 public class Tests {
 
-	
-
-	public static void main(String[] args) throws Throwable {
-
-
-		System.out.println("Test de parsage JSON");
+	public static ArrayList<Joueur> parsageJSONInfosJoueurs(){
 		ArrayList<Joueur> liste = new ArrayList<Joueur>();
 		String jsonFile = "{ \n\"joueurs\":[\n {\n \"id\":1,\n \"pseudo\":\"Fake\",\n \"pays\":\"FRA\",\n \"armees_restantes\":10,\n \"cases_controlees\":3 },\n {\n \"id\":2,\n \"pseudo\":\"Dummy\",\n \"pays\":\"GBR\",\n \"armees_restantes\":5,\n \"cases_controlees\":2 }\n ],\n \"nb_joueurs\":2\n }";
 		JSONObject obj = new JSONObject(jsonFile);
 				JSONArray listeJoueurs = obj.getJSONArray("joueurs");
 				for(int i=0; i<listeJoueurs.length();i++){
-					System.out.println("Test du joueur "+i);
-					System.out.println("ID du joueur :"+listeJoueurs.getJSONObject(i).getInt("id")+"\n");
-					System.out.println("Pseudo du joueur: "+listeJoueurs.getJSONObject(i).getString("pseudo")+"\n");
-					System.out.println("armees_restantes du joueur: "+listeJoueurs.getJSONObject(i).getInt("armees_restantes")+"\n");
-					System.out.println("cases_controlees du joueur: "+listeJoueurs.getJSONObject(i).getInt("cases_controlees")+"\n");
-					liste.add(new Joueur(listeJoueurs.getJSONObject(i).getInt("id"),listeJoueurs.getJSONObject(i).getString("pseudo"),listeJoueurs.getJSONObject(i).getString("pseudo"),listeJoueurs.getJSONObject(i).getInt("armees_restantes"),listeJoueurs.getJSONObject(i).getInt("cases_controlees")));
+					liste.add(new Joueur(listeJoueurs.getJSONObject(i).getInt("id"),listeJoueurs.getJSONObject(i).getString("pseudo"),listeJoueurs.getJSONObject(i).getString("pays"),listeJoueurs.getJSONObject(i).getInt("armees_restantes"),listeJoueurs.getJSONObject(i).getInt("cases_controlees")));
 				}
+		return liste;
+
 	}
+
+	public static void main(String[] args) throws Throwable {
+
+		ArrayList<Joueur> infosJoueurs = parsageJSONInfosJoueurs();
+		assert infosJoueurs.get(0).id==1 : "L'id du premier joueur vaut 1";
+		assert infosJoueurs.get(0).pseudo.equals("Fake") : "Le pseudo du premier joueur vaut Fake";
+		assert infosJoueurs.get(0).pays.equals("FRA") : "Le pays du premier joueur vaut FRA";
+		assert infosJoueurs.get(0).armees_restantes==10 : "Le nombre d'armees_restantes du premier joueur vaut 10";
+		assert infosJoueurs.get(0).cases_controlees==3	 : "Le nombre de cases_controlees du premier joueur vaut 3";
+		assert infosJoueurs.get(1).id==2 : "L'id du second joueur vaut 2";
+		assert infosJoueurs.get(1).pseudo.equals("Dummy") : "Le pseudo du second joueur vaut Dummy";
+		assert infosJoueurs.get(1).pays.equals("GBR") : "Le pays du second joueur vaut GBR";
+		assert infosJoueurs.get(1).armees_restantes==5 : "Le nombre d'armees_restantes du second joueur vaut 5";
+		assert infosJoueurs.get(1).cases_controlees==2	 : "Le nombre de cases_controlees du second joueur vaut 2";
+		
+	}
+	
 
 }
