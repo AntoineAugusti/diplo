d77766889ec99e79a282e846ad79edf02b247403
Antoine Augusti <antoine@augusti.fr>
2015-04-18 18:54:06 +0200

    PSR-2 pour les routes

--- application/serveur/app/Http/routes.php
+++ application/serveur/app/Http/routes.php
@@ -5,7 +5,6 @@ Route::get('parties/{partie}/statut', 'PartiesController@getStatut');
 Route::get('parties/{partie}/joueurs', 'PartiesController@getJoueurs');
 Route::post('parties/{partie}/rejoindre', 'PartiesController@postRejoindre');
 
-Route::post('queue/receive', function()
-{
+Route::post('queue/receive', function () {
     return Queue::marshal();
 });
