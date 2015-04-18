992dd027b6367d55d740550e906cbd5c6952daa5
Antoine Augusti <antoine@augusti.fr>
2015-04-18 15:26:14 +0200

    Préparation à l'utilisation de iron.io

--- application/serveur/app/Http/routes.php
+++ application/serveur/app/Http/routes.php
@@ -1,7 +1,11 @@
 <?php
 
-
 Route::get('/', 'WelcomeController@showWelcome');
 Route::get('parties/{partie}/statut', 'PartiesController@getStatut');
 Route::get('parties/{partie}/joueurs', 'PartiesController@getJoueurs');
 Route::post('parties/{partie}/rejoindre', 'PartiesController@postRejoindre');
+
+Route::post('queue/receive', function()
+{
+    return Queue::marshal();
+});
