2a4ddc06daaa64a2e65fdfcf16f8010fe79e8ed2
Antoine Augusti <antoine@augusti.fr>
2015-04-19 14:41:00 +0200

    Docblocks pour les routes

--- application/serveur/app/Http/routes.php
+++ application/serveur/app/Http/routes.php
@@ -1,13 +1,19 @@
 <?php
 
+// Accueil
 Route::get('/', 'WelcomeController@showWelcome');
+
+// Conversations
 Route::post('conversations', 'ConversationsController@postConversations');
 Route::get('conversations/{conversation}', 'ConversationsController@getConversation');
+
+// Parties
 Route::get('parties/{partie}/statut', 'PartiesController@getStatut');
 Route::get('parties/{partie}/joueurs', 'PartiesController@getJoueurs');
 Route::get('parties/{partie}/phase', 'PartiesController@getPhase');
 Route::post('parties/{partie}/rejoindre', 'PartiesController@postRejoindre');
 
+// Queues
 Route::post('queue/receive', function () {
     return Queue::marshal();
 });
