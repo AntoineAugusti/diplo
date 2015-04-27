6e48a88eab61510aab2d8a043aa50c6c1a34d11e
Nathan Malo <nathan.malo@gmail.com>
2015-04-27 12:03:00 +0200

    Ajout du lien vers une librairie nécessaire.

--- application/client/execTests.sh
+++ application/client/execTests.sh
@@ -1,2 +1,2 @@
 java -ea -cp classes/:lib/json-20140107.jar fr.insarouen.asi.diplo.JSONTests 
-java -ea -cp classes fr.insarouen.asi.diplo.ComTests
+java -ea -cp classes/:lib/json-20140107.jar fr.insarouen.asi.diplo.ComTests
