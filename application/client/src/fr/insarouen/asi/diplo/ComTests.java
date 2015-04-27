1d5e80ae9ea01822038c07b2d42e6f6ec89763ce
Nathan Malo <nathan.malo@gmail.com>
2015-04-27 15:55:20 +0200

    Ajout des tests

--- application/client/src/fr/insarouen/asi/diplo/ComTests.java
+++ application/client/src/fr/insarouen/asi/diplo/ComTests.java
@@ -2,6 +2,7 @@ package fr.insarouen.asi.diplo;
 
 import fr.insarouen.asi.diplo.Reseau.CommunicationServeur;
 import fr.insarouen.asi.diplo.MoteurJeu.Jeu;
+import fr.insarouen.asi.diplo.MoteurJeu.Ordres.*;
 import fr.insarouen.asi.diplo.Exception.ReseauException.*;
 import fr.insarouen.asi.diplo.MoteurJeu.*;
 import org.json.*;
@@ -18,69 +19,49 @@ import java.io.IOException;
 
 
 public class ComTests{
-
-	public static String requeteGet(){
-		String line ="";
-		String reponse="";
-		try{
-			URL url = new URL("https://api.diplo-lejeu.fr/parties/1/joueurs");
-			HttpsURLConnection httpsConnection = (HttpsURLConnection) url.openConnection();
-			httpsConnection.setRequestMethod("GET");
-			httpsConnection.setRequestProperty("User-Agent", "Diplo/1.0");
-			httpsConnection.setRequestProperty("Content-Type","application/json");
-			
-			BufferedReader br = new BufferedReader(new InputStreamReader((httpsConnection.getInputStream())));
-			while ((line=br.readLine()) != null){
-			reponse +=line;
-			}
-			httpsConnection.disconnect();
-
-		}
-		catch(MalformedURLException e){
-			System.out.println(" L URL du serveur est invalide :"+e.getMessage());
-		}
-		catch(ProtocolException e){
-			System.out.println(" Le protocole utilisé est invalide :"+e.getMessage());
-		}
-		catch(IOException e){
-			System.out.println(" Problème d'E/S :"+e.getMessage());
-		}
-		return reponse;
-	}
-	public static String postHttpsContent(String urlString) throws IOException {
-	  String response="";
-	  URL url=new URL(urlString);
-	  HttpsURLConnection httpsConnection=(HttpsURLConnection)url.openConnection();
-	  httpsConnection.setRequestMethod("POST");
-	  httpsConnection.setDoInput(true);
-	  httpsConnection.setDoOutput(true);
-	  httpsConnection.setUseCaches(false);
-	  httpsConnection.setRequestProperty("User-Agent", "Diplo/1.0");
-	  httpsConnection.setRequestProperty("Content-Type","application/json");
-	  String postData="";
-	  DataOutputStream postOut=new DataOutputStream(httpsConnection.getOutputStream());
-	  postOut.writeBytes(postData);
-	  postOut.flush();
-	  postOut.close();
-	  int responseCode=httpsConnection.getResponseCode();
-	  System.out.println(responseCode);
-	  if (responseCode == 201) {
-	    String line;
-	    BufferedReader br=new BufferedReader(new InputStreamReader(httpsConnection.getInputStream()));
-	    while ((line=br.readLine()) != null) {
-	    	System.out.println("new line");
-	      response+=line;
-	      System.out.println(line);
-	    }
-	  }
-	  return response;
-	}
 	
 	public static void main(String[] args) throws Throwable {
 		CommunicationServeur current = new CommunicationServeur("https://api.diplo-lejeu.fr/");
-		// Jeu jeu = current.rejoindrePartie(1);
-		// assert jeu.getID()==1 : "id du jeu";
-		// assert jeu.getRequis()==5 : "requis";
+		
+		Jeu jeu = current.rejoindrePartie(1);
+		assert jeu.getID()==1 : "ID du jeu vaut 1";
+		assert jeu.getRequis()==5 : "5 joueurs requis";
+		assert jeu.getInscrits()>=3 : "Au moins 3 joueurs inscrits";
+
+		Joueur local = (Joueur) jeu.getJoueurs().values().toArray()[0];
+		assert local.pseudo != null : "Le nom est non null";
+		assert local.id > 0 : "L'id est non null";
+		assert local.pays != null : "Le pays est non null";
+		assert local.armees_restantes == 0 : "Les armees_restantes est non null";
+		assert local.cases_controlees == 0 : "Le joueur n'a pas de cases";
+
+		ArrayList<Joueur> joueurs = current.recupererInfosJoueurs(1);
+		assert joueurs.size()>=3 : "Il y a au moins 3 joueurs";
+		for(int i = 0; i<joueurs.size();i++){
+			local = joueurs.get(i);
+			assert local.pseudo != null : "Le nom est non null";
+			assert local.id > 0 : "L'id est non null";
+			assert local.pays != null : "Le pays est non null";
+			assert local.armees_restantes == 0 : "Les armees_restantes est non null";
+			assert local.cases_controlees == 0 : "Le joueur n'a pas de cases";
+		}
+
+		String reponse = current.recupererInfosPartie(1);
+		assert !reponse.equals("En cours") : "L etat de la partie ne vaut pas En cours";
+
+		// Phase phaseCourante = current.recupererInfosPhase(1);
+		// assert phaseCourante.getStatutPhaseCourante() != null : "le statut est non null";
+		// assert phaseCourante.getChrono() > 0 : "le chrono est non null";
+
+		// Carte carte = current.recupererInfosCarte(1);
+		// assert carte instanceof Carte : "On a bien un type carte";
+		// for (Case c : carte.getCases()){
+		// 	assert c.id >=0 : "L'id est >=0";
+		// 	assert c.id_joueur >=0 : "L'id du joueur est plus grand ou egal a 0";
+		// 	assert c.id_armee >=0 : "L'id_armee est non nul";
+		// }
+
+
 
 	}
 }
