c067e87b069f9aa7c4f3bc6e16267f99f468cad8
Jean-Claude Bernard <jeanclaude.bernard@insa-rouen.fr>
2015-04-28 14:06:08 +0200

    Génération d'une couleur aléatoire pour un joueur

---
+++ application/client/src/fr/insarouen/asi/diplo/MoteurJeu/Couleur.java
@@ -0,0 +1,39 @@
+package fr.insarouen.asi.diplo.MoteurJeu;
+
+import java.util.*;
+
+public class Couleur {
+
+	public Couleur() {
+
+	}
+
+	private int[] permutationDeFischerYatts(int[] tableau, int dimension) {
+		int resultat[] = new int[dimension];
+		Random rand = new Random();
+		int indice = 0;
+		int max = tableau.length-1; 
+
+		while(indice<resultat.length && max != 0) {
+			int ind = rand.nextInt(max);
+			int temp = tableau[ind];
+			tableau[ind] = tableau[max];
+			tableau[max] =  temp;
+			resultat[indice] = tableau[max];
+			max = max - 1;
+			indice++;
+		}
+		resultat[indice] = tableau[max];
+		return resultat;
+	}
+
+	public int[] genererPiscineDeCouleur() {
+		int dimension = 5;
+		int couleur[] = new int[dimension];
+		int piscineDeCouleur[] = new int[] {41, 43, 44, 45, 46};
+
+		couleur = permutationDeFischerYatts(piscineDeCouleur, dimension);
+
+		return couleur;
+	}
+}
