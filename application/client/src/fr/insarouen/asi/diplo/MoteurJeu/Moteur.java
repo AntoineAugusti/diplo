cb27bfecbd306ea0d549ef9340a1912d53efa1e7
Manon Ansart <manon.ansart@gmail.com>
2015-04-29 16:21:53 +0200

    Ajout des fonctions de bases du moteur

--- application/client/src/fr/insarouen/asi/diplo/MoteurJeu/Moteur.java
+++ application/client/src/fr/insarouen/asi/diplo/MoteurJeu/Moteur.java
@@ -1,9 +1,60 @@
 package fr.insarouen.asi.diplo.MoteurJeu;
 
+import fr.insarouen.asi.diplo.Exception.ReseauException.*;
+import fr.insarouen.asi.diplo.Exception.OrdresException.*;
+import fr.insarouen.asi.diplo.MoteurJeu.Negociation.*;
+import fr.insarouen.asi.diplo.MoteurJeu.Ordres.*;
 import fr.insarouen.asi.diplo.Reseau.CommunicationServeur;
+import java.util.ArrayList;
 
 public class Moteur{
-	public Moteur(){
+
+	public CommunicationServeur current;
+
+	
+	public Jeu rejoindrePartie(int partieID) throws RuntimeException, PartieIntrouvableException, PartiePleineException{
+		
+		return current.rejoindrePartie(partieID);
+	}
+
+	public ArrayList<Joueur> recupererInfosJoueurs(int partieID) throws PartieIntrouvableException, RuntimeException{
+		return current.recupererInfosJoueurs(partieID);
+	}
+
+	public String recupererInfosPartie(int partieID) throws PartieIntrouvableException, RuntimeException{
+		return current.recupererInfosPartie(partieID);
+	}
+
+	public Phase recupererInfosPhase(int partieID) throws PartieIntrouvableException, PartieInvalideException, RuntimeException{
+		return current.recupererInfosPhase(partieID);
+	}
+
+	public Carte recupererInfosCarte(int partieID) throws PartieIntrouvableException, RuntimeException{
+		return current.recupererInfosCarte(partieID);
+	}
+
+	public Boolean posterOrdre(int partieID, Ordre ordre)throws OrdreInvalideException{
+		return current.posterOrdre(partieID, ordre);
+	}
+
+	public Conversation recupererInfosConversation(int conversationID) throws PartieIntrouvableException, RuntimeException{
+		return current.recupererInfosConversation(conversationID);
+	}
+
+	public ArrayList<Conversation> recupererInfosConversationSelonJoueur(int joueurID) throws PartieIntrouvableException{
+		return current.recupererInfosConversationSelonJoueur(joueurID);
+	}
+
+	public Conversation creerConversation(ArrayList<Integer> destinataires) throws PartieInvalideException{
+		return current.creerConversation(destinataires);
+	}
+
+	public Message posterMessage(int conversationID, int auteur, String texte) throws PartieInvalideException{
+		return current.posterMessage(conversationID, auteur, texte);
+	}
+
+	public Moteur(){	
+		current = new CommunicationServeur("https://api.diplo-lejeu.fr/");
 		
 		// Phase phase = new Phase();
 		// Jeu jeu = rejoindrePartie(1);
@@ -19,7 +70,6 @@ public class Moteur{
 		// 	phase = recupererInfosPhase(1);
 
 		// 	etat = recupererInfosPartie(1);
-		// }
-				
+		// }			
 	}
 }
