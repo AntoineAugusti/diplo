f687b0188d5c70baa187fac7749bb91550f9bc94
Nathan Malo <nathan.malo@gmail.com>
2015-05-04 14:51:14 +0200

    Ajout des cases voisines

--- application/client/src/fr/insarouen/asi/diplo/MoteurJeu/Case.java
+++ application/client/src/fr/insarouen/asi/diplo/MoteurJeu/Case.java
@@ -6,12 +6,14 @@ public class Case{
 	public int id_joueur;
 	public boolean est_occupee;
 	public int id_armee;
+	public ArrayList<Case> cases_voisines;
 
-	public Case(int id, boolean est_libre, int id_joueur, boolean est_occupee, int id_armee){
+	public Case(int id, boolean est_libre, int id_joueur, boolean est_occupee, int id_armee, ArrayList<Case> voisines){
 		this.id = id;
 		this.est_libre = est_libre;
 		this.id_joueur = id_joueur;
 		this.est_occupee = est_occupee;
 		this.id_armee = id_armee;
+		this.cases_voisines = voisines;
 	}
 }
