fada66493b030bdc31fe520ed3592a58f92db4a8
Jean-Claude Bernard <jeanclaude.bernard@insa-rouen.fr>
2015-05-09 10:00:28 +0200

    feat(client) : ajout de getter et setter dans Armee

--- application/client/src/fr/insarouen/asi/diplo/MoteurJeu/Armee.java
+++ application/client/src/fr/insarouen/asi/diplo/MoteurJeu/Armee.java
@@ -3,13 +3,35 @@ package fr.insarouen.asi.diplo.MoteurJeu;
 import java.util.*;
 
 public class Armee {
-	public int id;
-	public int id_joueur;
-	public int id_case_courante;
+	private int id;
+	private int id_joueur;
+	private int id_case_courante;
+	private String pion_armee
 
 	public Armee(int id, int joueur, int id_case) {
 		this.id = id;
 		this.id_joueur = joueur;
 		this.id_case_courante = id_case;
 	}
+
+	public int getID() {
+		return this.id;
+	}
+
+	public int getJoueur() {
+		return this.id_joueur;
+	}
+
+	public getIdCaseCourante() {
+		return this.id_case_courante;
+	}
+
+	public void setPion(String pion) {
+		this.pion_armee = pion;
+	}
+
+	public String getPion() {
+		return this.pion_armee;
+	}
+
 }
